7800002cf1903d621280d80c1563ae34f5828b0b
Santiago Sala <aa2793@student.jamk.fi>
2022-10-16 00:22:15 +0300

    week 5: exercise 7: Finished

--- Week5/week5_SantiagoSala_Exercise7.py
+++ Week5/week5_SantiagoSala_Exercise7.py
@@ -4,7 +4,22 @@ print("Santiago Sala")
 print("Week 5 Exercise 7: Finnish - Spanish dictionary")
 print("This program will store some finnish to spanish words in a dictionary.\n")
 
+finspa_dict = {
+    "moi": "hola",
+    "heippa": "adios",
+    "koti": "casa",
+    "numero": "numero",
+    "puu": "arbol",
+    "jarvi": "lago",
+    "peruna": "patata",
+    "näkki": "salsicha",
+    "kirjasto": "biblioteca",
+    "auto": "cotxe",
+    "plkupyörä" : "bicicleta",
+    }
 
-
+print(f"The Finnish-Spanish dictionary contains: {finspa_dict} \n")
+print(f"The keys (finnish words) of the dictionary are: {finspa_dict.keys()}\n")
+print(f"The values (spanish words) of the dictionary are: {finspa_dict.values()}")
 
 print("\nThank you for playing.")
---
+++ Week5/week5_SantiagoSala_Exercise8.py
@@ -0,0 +1,7 @@
+import random
+
+print("Santiago Sala")
+print("Week 5 Exercise 8: Calculating standard deviation")
+print("This program will calculate the standard deviation of a random array with 20 elements. \n")
+
+print("\nThank you for playing.")
