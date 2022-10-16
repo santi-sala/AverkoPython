7b43ebf9362af5ab470a2a603b42d929a962bd03
Santiago Sala <aa2793@student.jamk.fi>
2022-10-16 23:02:23 +0300

    week 6 exercise 5 --> Finished

--- Week6/week6_SantiagoSala_Exercise5.py
+++ Week6/week6_SantiagoSala_Exercise5.py
@@ -18,12 +18,13 @@ def createNumbersArray(array_items):
     for x in range(array_items):
         new_item = is_int(f"This is the {x + 1} number out of {array_items}. Type a number. ")
         my_numbers.append(new_item)
-        
-    my_numbers.sort(reverse = True)
     return my_numbers
 
 numbers_array = createNumbersArray(is_int("How many numbers do you have? "))
 
-print(f"The biggest number of the given numbers ({numbers_array}) is: {numbers_array[0]}")
+result_array = numbers_array.copy()
+result_array.sort(reverse = True)
+
+print(f"The biggest number of the given numbers ({numbers_array}) is: {result_array[0]}")
 
 print("\nThank you for playing.")
