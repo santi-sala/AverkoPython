e26bd0c62c8124b35cfd840a581a0037275db871
Santiago Sala <aa2793@student.jamk.fi>
2022-10-19 00:16:17 +0300

    Week 4 exercise 6 --> Finished....

--- Week4/week4_SantiagoSala_Exrcise6.py
+++ Week4/week4_SantiagoSala_Exrcise6.py
@@ -1,19 +1,18 @@
+from sympy import symbols, Eq, solve
 import math
 print("Santiago Sala")
 print("Week 4 Exercise 6: Solve equation")
-print("This program will solve a equation give x")
-
-while True:
-    x = input("To proceed, what is the value of X: ")    
-    try:
-        val_x = float(x)
-        break;
-    except ValueError:
-        print("This is not a number. Please enter a valid number")
-
-solution_one = 3*val_x**3-4*val_x**2+9*val_x+5
-solution = math.sqrt(solution_one)
+print("This program will solve a given equation.")
 
 
-print(f'If X is {val_x}, the answer is: {solution_one} and its aquare root is: {round(solution, 2)}')
+x = symbols('x')
+eq1 = Eq(3*x*x*x-4*x*x+9*x+5, x)
+
+sol = solve(eq1, dict=True)
+
+print("The solutions are: \n")
+print(f"{sol[0]}\n")
+print(f"{sol[1]}\n")
+print(f"{sol[2]}\n")
+
 print("Thank you for playing")
